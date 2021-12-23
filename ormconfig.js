7b6b7c8c6e536dcf1377e8675e300ab58cace7f6
STUMPF1988 <stumpfdaniele@gmail.com>
2021-12-22 23:12:54 -0300

    decimo_segundo_commit

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
     type: "postgres",
     host: "ec2-52-70-107-254.compute-1.amazonaws.com",
     port: 5432,
-    username: "tsjihynoujkskq",
-    password: "8946ae1122402a187aa22cde25de6a611f9f123a2ea223b528f8eb89bfe4c5dd",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "d1c3k16rqqiq9h",
     synchronize: false,
     logging: false,
